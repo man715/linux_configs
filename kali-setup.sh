5eb5e0c1a1412179894e5ccc73d061309011b13a
man715 <pattonarron@gmail.com>
2021-04-08 19:16:35 -0600

    Use sudo instead of requiring the script to be ran with sudo

--- kali-setup.sh
+++ kali-setup.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-        echo "Needs to be ran as root!"
-        exit 1
-fi
-# Start ssh 
-echo [*] Starting SSH and enabling it on startup.
-systemctl enable ssh
-systemctl start ssh
-
-# Install and start xrdp
-echo [*] Installing, starting, and enabling on startup: XRDP.
-apt update && apt install -y xrdp
-systemctl enable xrdp
-systemclt start xrdp
-
 # Setup Config files
 echo [*] Getting config files for zsh, bash, and vim
 wget https://raw.githubusercontent.com/man715/linux_configs/main/.tmux.conf -O ~/.tmux.conf
@@ -22,6 +7,16 @@ wget https://raw.githubusercontent.com/man715/linux_configs/main/.zshrc -O ~/.zs
 source ~/.zshrc
 wget https://raw.githubusercontent.com/man715/linux_configs/main/.vimrc -O ~/.vimrc
 
+# Start ssh 
+echo [*] Starting SSH and enabling it on startup.
+sudo systemctl enable ssh
+sudo systemctl start ssh
+
+# Install and start xrdp
+echo [*] Installing, starting, and enabling on startup: XRDP.
+sudo apt update && apt install -y xrdp
+sudo systemctl enable xrdp
+sudo systemclt start xrdp
 
 # Install tmux and vim plugin managers
 echo [*] Installing tmux and vim plugin managers
@@ -31,31 +26,31 @@ echo [!]You will need to open vim and run :PlugInstall!
 
 # Installing Tools
 echo [*] Installing hacking tools
-apt install -y seclists
-apt install -y gobuster
-apt install -y mingw-w64
-apt install -y shellter
-apt install -y wine
-apt install -y crowbar
-searchsploit -u
+sudo apt install -y seclists
+sudo apt install -y gobuster
+sudo apt install -y mingw-w64
+sudo apt install -y shellter
+sudo apt install -y wine
+sudo apt install -y crowbar
+sudo searchsploit -u
 
 # Unzip rockyou.txt
 echo [*] Unzip rockyou.txt.gz
-gunzip /usr/share/wordlists/rockyou.txt.gz
+sudo gunzip /usr/share/wordlists/rockyou.txt.gz
 
 #Fix the encoding of rockyou.txt
 echo [*] Fix the rockyou.txt encoding
 echo [~} https://github.com/zaproxy/zaproxy/issues/6002
-iconv -f iso-8859-15 -t utf8 /usr/share/wordlists/rockyou.txt > rockyou-utf8.txt
-mv ./rockyou-utf8.txt /usr/share/wordlists/
+sudo iconv -f iso-8859-15 -t utf8 /usr/share/wordlists/rockyou.txt > rockyou-utf8.txt
+sudo mv ./rockyou-utf8.txt /usr/share/wordlists/
 
 # Setting up python environment
 echo [*] Install Python environment
 echo [*] Install dependencies
-apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
+sudo apt install -y build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
 
 echo [*] Download the pyenv script and run it
-curl https://pyenv.run | zsh
+sudo curl https://pyenv.run | zsh
 echo [*] PyEnv is installed
 echo [*] Go here for some simple usage instructions:
 echo [~] https://www.kali.org/docs/general-use/using-eol-python-versions/
